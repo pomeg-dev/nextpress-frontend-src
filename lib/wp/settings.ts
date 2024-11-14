f577ad6cc4163ea1ee9175fcc444405009cb8efc
Vic Lobins <vic.l@pomegranate.co.uk>
2024-11-14 17:08:05 +0000

    added setting api endpoint

---
+++ lib/wp/settings.ts
@@ -0,0 +1,19 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function getSettings() {
+  const url = `${API_URL}/wp-json/nextpress/settings`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    next: { tags: ["settings"] },
+    cache: "no-cache",
+  });
+
+  if (!response.ok) {
+    console.log(url);
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const res = await response.json();
+  return res;
+}
