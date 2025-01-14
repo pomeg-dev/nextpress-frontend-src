c38d3d7f0d19930f659fb489f24425f99b9518fa
Vic Lobins <vic.l@pomegranate.co.uk>
2025-01-14 09:46:08 +0000

    404 sorking

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,12 @@
-import { getPostByPath } from "@/lib/wp/posts";
+import { getDefaultTemplate, getPostByPath } from "@/lib/wp/posts";
 import { getSettings } from "@/lib/wp/settings";
 import { BlockParser } from "@/ui/block-parser";
 import classNames from "classnames";
+import BeforeContent from "./BeforeContent";
+import { Styles } from "./(extras)/styles";
+import AfterContent from "./AfterContent";
+import { Suspense } from "react";
+import { GTM } from "./(extras)/gtm";
 
 type PageProps = {
   params: {
@@ -11,10 +16,13 @@ type PageProps = {
 
 export default async function NotFound() {
   const settings = await getSettings();
+  const defaultTemplate = await getDefaultTemplate();
 
   if (!settings.page_404) {
     return (
       <>
+        <BeforeContent defaultTemplate={defaultTemplate} />
+        <Styles settings={settings} />
         <div className="page-404 flex min-h-screen flex-col justify-between">
           <div
             className={classNames(
@@ -28,88 +36,32 @@ export default async function NotFound() {
             </div>
           </div>
         </div>
+        <AfterContent defaultTemplate={defaultTemplate} />
       </>
     );
   } else {
-    const post = await getPostByPath(settings.page_404.post_name, true, true);
+    const post = await getPostByPath(settings.page_404.post_name, true, false);
     return (
-      <main data-pageurl={settings.page_404.post_name} data-postid={settings.page_404.ID}>
-        {post.content && <BlockParser blocks={post.content} />}
-      </main>
+      <body className="no-transition">
+        {settings.google_tag_manager_enabled === true && (
+          <Suspense>
+            <GTM GTM_ID={settings.google_tag_manager_id} />
+          </Suspense>
+        )}
+        <BeforeContent defaultTemplate={defaultTemplate} />
+        <Styles settings={settings} />
+        <main data-pageurl={post.path} data-postid={post.id}>
+          {post.content && <BlockParser blocks={post.content} />}
+        </main>
+        <AfterContent defaultTemplate={defaultTemplate} />
+      </body>
     );
   }
 }
 
-// import {
-//   getAllPostsArray,
-//   getPage,
-//   getSettings,
-//   getSettingsByPostId,
-// } from "lib/api";
-// import cn from "classnames";
-// import { Styles } from "./(extras)/styles";
-// const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-// type PageProps = {
-//   params: {
-//     slug: string;
-//   };
-// };
-
-// export default async function NotFound() {
-//   const allPosts = await getAllPostsArray("not-found");
-//   const settings = await getSettings("not-found");
-//   //if coming soon page is enabled and user is not logged in, show the coming soon page
-//   if (!settings.page_404)
-//     return (
-//       <>
-//         <div className="page-404 flex min-h-screen flex-col justify-between">
-//           {/* @ts-expect-error Server Component */}
-//           <Header settings={settings} />
-//           <div
-//             className={cn(
-//               "overflow-hidden bg-white",
-//               "button-effect-" + settings.btn_transition
-//             )}
-//           >
-//             <div className="py-[100px] text-center">
-//               <h1 className="text-[50px] font-bold">404</h1>
-//               <p className="text-[20px]">Page not found</p>
-//             </div>
-//           </div>
-//           {/* @ts-expect-error Server Component */}
-//           <Footer settings={settings} />
-//         </div>
-//         <Styles settings={settings} />
-//       </>
-//     );
-//   const page404 = await getPage(settings.page_404.ID);
-//   return (
-//     <div className="page-404">
-//       {/* @ts-expect-error Server Component */}
-//       <Header settings={settings} />
-//       <div
-//         className={cn(
-//           "overflow-hidden bg-white",
-//           "button-effect-" + settings.btn_transition
-//         )}
-//       >
-//         <BlockContent
-//           settings={settings}
-//           blockData={page404.block_data}
-//           allPosts={allPosts}
-//         />
-//       </div>
-//       {/* @ts-expect-error Server Component */}
-//       <Footer settings={settings} />
-//       <Styles settings={settings} />
-//     </div>
-//   );
-// }
-
-// export async function generateMetadata(props: PageProps) {
-//   const settings = await getSettings();
-//   if (!settings.page_404) return null;
-//   const page404 = await getPage(settings.page_404.ID);
-//   return page404.yoast_head_json;
-// }
+export async function generateMetadata(props: PageProps) {
+  const settings = await getSettings();
+  if (!settings.page_404) return null;
+  const page404 = await getPostByPath(settings.page_404.post_name);
+  return page404.yoastHeadJSON;
+}
