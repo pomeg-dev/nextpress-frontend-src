c6c7caf1707000c50a4671e4ceffa7e349a9a7b9
pome-developer <developer@pomegranate.co.uk>
2025-01-16 17:32:03 +0000

    adding vercel outbound Ip log

--- app/api/sql/route.ts
+++ app/api/sql/route.ts
@@ -52,6 +52,7 @@ export async function GET(request: NextRequest) {
 
   try {
     const data = await getMysqlData(sql);
+    console.log("Vercel outbound IP:", data.ip);
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: { "Content-Type": "application/json" },
