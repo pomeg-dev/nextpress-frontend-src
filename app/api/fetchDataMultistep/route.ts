1ca143c5e9d1c2315400759db8fa44e6f1dec248
pome-developer <developer@pomegranate.co.uk>
2025-01-28 07:42:58 +0100

    cleaning up ai routes etc

--- app/api/fetchDataMultistep/route.ts
+++ app/api/fetchDataMultistep/route.ts
@@ -1,4 +1,3 @@
-// app/api/customer/multistep/route.ts
 import { getMysqlData } from "@/lib/server/mysql";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -13,6 +12,25 @@ export async function GET(request: NextRequest) {
     );
   }
 
+  try {
+    const customerInfo = await fetchDataMultistep(aban8);
+    if (!customerInfo) {
+      return NextResponse.json(null);
+    }
+
+    return NextResponse.json(customerInfo);
+  } catch (error) {
+    console.error("Error fetching customer data:", error);
+    return NextResponse.json(
+      {
+        error: "Error fetching customer data, please contact the support team.",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+async function fetchDataMultistep(aban8: string) {
   const sqlQuery = `
     WITH AccountType AS (
       SELECT 
@@ -72,73 +90,94 @@ export async function GET(request: NextRequest) {
     FROM AllRelatedAccounts;
   `;
 
+  const sqlResult = await getMysqlData(sqlQuery);
+  if (!sqlResult || sqlResult.length < 1) return null;
+
+  const data = sqlResult[0];
+  data.current_account = null;
+
+  if (data.account_type !== "Individual" && data.related_accounts) {
+    const relatedAccData = await fetchCustomerData(data.related_accounts);
+    if (relatedAccData) {
+      const currentAccIndex = relatedAccData.findIndex(
+        (item: { aban8: string }) => item.aban8 == aban8
+      );
+      data.current_account = relatedAccData[currentAccIndex];
+      data.related_accounts = relatedAccData;
+    }
+  } else {
+    const indData = await fetchCustomerData([
+      { related_aban8: aban8, related_account_type: "Individual" },
+    ]);
+    if (indData[0]) {
+      data.current_account = indData[0];
+      data.related_accounts = null;
+    }
+  }
+
+  // Set tiers
+  const tierBoxes = data.current_account?.ppp_tier.replace("P", "");
+  const currentTierIndex = tiers.findIndex(
+    (item: { number_of_boxes: number }) => item.number_of_boxes >= tierBoxes
+  );
+
+  const currentTier = currentTierIndex !== -1 ? tiers[currentTierIndex] : null;
+  const nextTier =
+    currentTierIndex + 1 < tiers.length ? tiers[currentTierIndex + 1] : null;
+  const nextNextTier =
+    currentTierIndex + 2 < tiers.length ? tiers[currentTierIndex + 2] : null;
+
+  return {
+    accountType: data.account_type,
+    currentAccount: data.current_account,
+    relatedAccounts: data.related_accounts,
+    currentTier,
+    targetTier: nextTier,
+    futureTier: nextNextTier,
+  };
+}
+
+async function fetchCustomerData(
+  aban8array: { related_aban8: string; related_account_type: string }[]
+) {
+  const aban8List = aban8array
+    .map((item: { related_aban8: any }) => item.related_aban8)
+    .join(", ");
+  const aban8Query = `
+    SELECT
+      c.aban8,
+      c.acct_name,
+      c.cust_name,
+      c.ppp_tier,
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+          'inv_dt', s.inv_dt,
+          'revenue', s.revenue,
+          'qty', s.qty
+        )
+      ) AS sales
+    FROM customers c
+    LEFT JOIN sales s
+      ON c.aban8 = s.aban8
+      AND s.inv_dt >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR)
+      AND s.product = '125-Arestin'
+    WHERE c.aban8 IN (${aban8List})
+    GROUP BY c.aban8, c.acct_name, c.cust_name, c.ppp_tier, c.email;
+  `;
+
   try {
-    const sqlResult = await getMysqlData(sqlQuery);
-    if (!sqlResult || sqlResult.length < 1) {
-      return NextResponse.json(null);
-    }
-
-    const data = sqlResult[0];
-    data.current_account = null;
-
-    if (data.account_type !== "Individual" && data.related_accounts) {
-      const relatedAccResponse = await fetch(
-        `${
-          process.env.NEXT_PUBLIC_FRONTEND_URL
-        }/fetchCustomerData?aban8List=${data.related_accounts
-          .map((acc: any) => acc.related_aban8)
-          .join(",")}`
-      );
-      const relatedAccData = await relatedAccResponse.json();
-
-      if (relatedAccData) {
-        const currentAccIndex = relatedAccData.findIndex(
-          (item: { aban8: string }) => item.aban8 == aban8
+    const aban8result = await getMysqlData(aban8Query);
+    if (aban8result) {
+      aban8result.forEach((account: { aban8: string; account_type: any }) => {
+        const match = aban8array.find(
+          (item) => item.related_aban8 == account.aban8
         );
-        data.current_account = relatedAccData[currentAccIndex];
-        data.related_accounts = relatedAccData;
-      }
-    } else {
-      const indResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_URL}/fetchCustomerData?aban8List=${aban8}`
-      );
-      const indData = await indResponse.json();
-
-      if (indData[0]) {
-        data.current_account = indData[0];
-        data.related_accounts = null;
-      }
+        account.account_type = match ? match.related_account_type : null;
+      });
+      return aban8result;
     }
-
-    // Set tiers
-    const tierBoxes = data.current_account?.ppp_tier.replace("P", "");
-    const currentTierIndex = tiers.findIndex(
-      (item: { number_of_boxes: number }) => item.number_of_boxes >= tierBoxes
-    );
-
-    const currentTier =
-      currentTierIndex !== -1 ? tiers[currentTierIndex] : null;
-    const nextTier =
-      currentTierIndex + 1 < tiers.length ? tiers[currentTierIndex + 1] : null;
-    const nextNextTier =
-      currentTierIndex + 2 < tiers.length ? tiers[currentTierIndex + 2] : null;
-
-    return NextResponse.json({
-      accountType: data.account_type,
-      currentAccount: data.current_account,
-      relatedAccounts: data.related_accounts,
-      currentTier,
-      targetTier: nextTier,
-      futureTier: nextNextTier,
-    });
   } catch (error) {
     console.error("Error fetching customer data:", error);
-    return NextResponse.json(
-      {
-        error: "Error fetching customer data, please contact the support team.",
-      },
-      { status: 500 }
-    );
   }
 }
 
