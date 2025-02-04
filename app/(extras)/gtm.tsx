cb330b4fe563ccaec9911e6501934a99a93095ca
Vic Lobins <vic.l@pomegranate.co.uk>
2025-02-04 10:02:01 +0000

    attempting optimised vwo, videoask and gtm scripts

--- app/(extras)/gtm.tsx
+++ app/(extras)/gtm.tsx
@@ -21,26 +21,17 @@ export function GTM({ GTM_ID }: { GTM_ID: string }) {
 
   return (
     <>
-      <noscript>
+      {/* <noscript>
         <iframe
           src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
         />
-      </noscript>
+      </noscript> */}
       <Script
-        id="gtm-script"
+        src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', '${GTM_ID}');
-  `,
-        }}
       />
     </>
   );
