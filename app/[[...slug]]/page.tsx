8e5844501ff35ae0df67a5c620e224779b0a6ae9
pome-developer <developer@pomegranate.co.uk>
2025-03-04 12:29:37 +0000

    remvoign sidebar styles main src

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -107,13 +107,7 @@ export default async function Post(props: NextProps) {
         <BeforeContent defaultTemplate={defaultTemplate} />
         <NPAdminBar postID={post.id} />
         <Styles settings={settings} />
-        <main
-          className={classNames(
-            post?.acf_data?.sidebar_menu && "w-[calc(100%-300px)] min-h-[calc(100vh-73px)] ml-[300px] bg-[rgb(245,248,249)]"
-          )}
-          data-pageurl={post.slug.slug}
-          data-postid={post.id}
-        >
+        <main data-pageurl={post.slug.slug} data-postid={post.id}>
           {post.content && <BlockParser blocks={post.content} />}
         </main>
         <AfterContent defaultTemplate={defaultTemplate} />
