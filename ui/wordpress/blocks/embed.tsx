48f0f850d635b626b7e4b24d40fc94bed25e7221
Vic Lobins <vic.l@pomegranate.co.uk>
2025-06-13 12:06:45 +0100

    fixed embed

--- ui/wordpress/blocks/embed.tsx
+++ ui/wordpress/blocks/embed.tsx
@@ -4,15 +4,25 @@ import classNames from "classnames";
 
 
 const Embed: React.FC<Block> = ({ ...block }: Block) => {
-  const { data } = block;
+  const { data, className } = block;
+  const isVideo = data?.url ? /vimeo|youtube|youtu\.be/i.test(data.url) : false;
   return (
     <figure className={classNames(
-      "wp-block-embed aspect-video relative my-4",
+      "wp-block-embed relative my-4",
       data?.providerNameSlug && `is-provider-${data.providerNameSlug}`,
       data?.type && `is-type-${data.type}`,
+      isVideo && "aspect-video",
+      className,
     )}>
       {data?.url &&
-        <iframe className="absolute h-full w-full" src={data.url} frameBorder="0"></iframe>
+        <iframe 
+          className={classNames(
+            "h-full w-full",
+            isVideo && "absolute"
+          )}
+          src={data.url} 
+          frameBorder="0">
+        </iframe>
       }
     </figure>
   );
