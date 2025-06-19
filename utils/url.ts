f4ff21238a475e1fed19a670751d49d7c51f1efd
Vic Lobins <vic.l@pomegranate.co.uk>
2025-06-19 17:01:42 +0100

    pdfs fix

--- utils/url.ts
+++ utils/url.ts
@@ -19,6 +19,7 @@ export function getBaseURL(url: string) {
 // function which is run on every link to replace wordpress urls with next ones (header/mobile nav and footer navs).
 export function linkFilter(linkUrl: string, API_URL: any) {
   if (!linkUrl) return "#";
+  if (linkUrl.includes('.pdf')) return linkUrl;
   let newLinkUrl = linkUrl;
   //first replace the API_URL with the base url
   newLinkUrl = linkUrl.replace(API_URL, "");
