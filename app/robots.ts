f0a736cc7d0ea37545958090e652ca0573eae57e
Vic Lobins <vic.l@pomegranate.co.uk>
2025-06-25 18:21:07 +0100

    screaming from

--- app/robots.ts
+++ app/robots.ts
@@ -2,13 +2,19 @@ import { MetadataRoute } from "next";
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
   return {
-    rules: {
-      // userAgent: "*",
-      // allow: "/",
-      // disallow: "/private/",
-      userAgent: "*",
-      disallow: "/"
-    },
+    rules: [
+      {
+        userAgent: "Screaming Frog SEO Spider",
+        allow: "/",
+      },
+      {
+        // userAgent: "*",
+        // allow: "/",
+        // disallow: "/private/",
+        userAgent: "*",
+        disallow: "/"
+      },
+    ],
     sitemap: `/sitemap.xml`,
   };
 }
