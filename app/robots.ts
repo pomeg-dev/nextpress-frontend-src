c8c8dc011043750760fc7bbe7195037c6d6586a6
Vic Lobins <vic.l@pomegranate.co.uk>
2025-07-15 11:28:50 +0100

    added more to robots

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,50 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
   return {
     rules: [
       {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "FirecrawlAgent",
+        allow: "/",
+      },
+      {
+        userAgent: "AndiBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ExaBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PhindBot",
+        allow: "/",
+      },
+      {
+        userAgent: "YouBot",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "CCBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Google-Extended",
+        disallow: "/",
+      },
+      {
         userAgent: "*",
         allow: "/",
         disallow: "/private/",
