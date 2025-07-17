e789529e3e45a46b9703e18a74f84b94546aaa1f
Vic Lobins <vic.l@pomegranate.co.uk>
2025-07-17 17:39:22 +0100

    settings keys added

---
+++ utils/settings-keys.ts
@@ -0,0 +1,18 @@
+export const SETTINGS_KEYS = [
+  'enable_user_flow',
+  'google_tag_manager_enabled', 
+  'google_tag_manager_id',
+  'enable_vwo',
+  'vwo_id',
+  'page_for_posts_slug',
+  'frontend_url',
+  'before_content',
+  'after_content',
+  'page_404',
+  'btn_transition',
+  'og_default_image',
+  'primary_color',
+  'secondary_color',
+  'blogname',
+  'favicon'
+];
