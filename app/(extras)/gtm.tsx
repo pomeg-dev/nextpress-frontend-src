c14fb5fd90ff3e2116ea0e38e6e2f1b20e07cea7
Vic Lobins <vic.l@pomegranate.co.uk>
2025-09-04 11:32:16 +0100

    added gtm init script

--- app/(extras)/gtm.tsx
+++ app/(extras)/gtm.tsx
@@ -5,9 +5,35 @@ import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
 import { useEffect } from "react";
 
+declare global {
+  interface Window {
+    dataLayer?: Object[];
+    gtag?: (...args: any[]) => void;
+  }
+}
+
 export function GTM({ GTM_ID }: { GTM_ID: string }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (window.dataLayer?.find(item => 
+      typeof item === 'object' && item !== null && 'gtm.start' in item
+    )) {
+      return;
+    }
+
+    window.dataLayer = window.dataLayer || [];
+
+    window.gtag = function gtag() {
+      window.dataLayer?.push(arguments);
+    };
+
+    window.dataLayer.push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+  }, []);
 
   useEffect(() => {
     if (pathname) {
